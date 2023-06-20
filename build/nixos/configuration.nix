d29f0c1d0cf2afc9d6eb31f95bfefa9ee9255934
Drewry Pope <drewrypope@gmail.com>
2023-06-20 00:18:22 -0500

    Create configuration.nix
    Signed-off-by: Drewry Pope <drewrypope@gmail.com>

---
+++ build/nixos/configuration.nix
@@ -0,0 +1,39 @@
+{ lib, pkgs, config, modulesPath, ... }:
+
+with lib;
+let
+  nixos-wsl = import ./nixos-wsl;
+in
+{
+  imports = [
+    "${modulesPath}/profiles/minimal.nix"
+
+    nixos-wsl.nixosModules.wsl
+  ];
+
+  wsl = {
+    enable = true;
+    automountPath = "/mnt";
+    defaultUser = "nixos";
+    startMenuLaunchers = true;
+
+    # Enable native Docker support
+    # docker-native.enable = true;
+
+    # Enable integration with Docker Desktop (needs to be installed)
+    # docker-desktop.enable = true;
+
+  };
+  services.nginx.enable = true;
+  systemd.services.nginx.serviceConfig.ProtectHome = lib.mkForce false;
+  systemd.services.nginx.serviceConfig.ProtectSystem = lib.mkForce false;
+  services.nginx.virtualHosts."localhost" = {
+      root = "/var/www/localhost";
+  };
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  system.stateVersion = "22.05";
+}
