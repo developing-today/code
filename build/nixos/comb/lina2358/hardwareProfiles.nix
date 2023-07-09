6b265ffa2900ced556d8117bb9a4762bccc5b288
Drewry Pope <drewrypope@gmail.com>
2023-07-08 19:37:53 -0500

    comment this out

--- build/nixos/comb/lina2358/hardwareProfiles.nix
+++ build/nixos/comb/lina2358/hardwareProfiles.nix
@@ -4,10 +4,10 @@
       inputs.disko.nixosModules.disko
       ./hardwareProfiles/lavinox/default.nix
       {disko.devices = import ./hardwareProfiles/lavinox/disko-config.nix;}
-      inputs.nixos-hardware.nixosModules.common-pc-laptop
+      /*inputs.nixos-hardware.nixosModules.common-pc-laptop
       inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
       inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-      inputs.nixos-hardware.nixosModules.common-gpu-amd
+      inputs.nixos-hardware.nixosModules.common-gpu-amd*/
     ];
   };
 }
