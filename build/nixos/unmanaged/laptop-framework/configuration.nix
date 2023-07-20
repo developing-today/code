988631e2c6a781838e77d29b52abcd64878072b0
Drewry Pope <drewrypope@gmail.com>
2023-07-20 01:24:39 -0500

    en

--- build/nixos/unmanaged/laptop-framework/configuration.nix
+++ build/nixos/unmanaged/laptop-framework/configuration.nix
@@ -16,14 +16,8 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  #networking.networkmanager.enable = true;
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/Chicago";
