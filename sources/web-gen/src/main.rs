2750780b8a2d14d9257f6e6951cfaabb07654f2e
Drewry Pope <drewrypope@gmail.com>
2023-07-20 03:42:48 -0500

    conditional classes

--- sources/web-gen/src/main.rs
+++ sources/web-gen/src/main.rs
@@ -6,12 +6,17 @@ fn main() {
 
 #[component]
 fn App(cx: Scope) -> impl IntoView {
-    let (count, set_count) = create_signal(cx, 0);
+    let default = 0;
+    let (count, set_count) = create_signal(cx, default);
 
     view! { cx,
-        <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        <button
+            class="bg-white hover:bg-gray-100 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+            class=("text-grey-800", move || count() == 0)
+            class=("text-blue-800", move || count() != 0 && count() % 2 == 0)
+            class=("text-green-800", move || count() % 2 != 0)
             on:click=move |_| {
-                set_count.update(|n| *n += if *n == 0 { 42 } else { 1 });
+                set_count.update(|n| *n += if *n == default { 42 } else { 1 });
             }
         >
             "Click me: "
