13389e39fad908591c23254e9ae088f4cb4ff01c
Drewry Pope <drewrypope@gmail.com>
2023-07-20 03:25:55 -0500

    tailwind htmx stuff

--- sources/web-gen/src/main.rs
+++ sources/web-gen/src/main.rs
@@ -9,7 +9,7 @@ fn App(cx: Scope) -> impl IntoView {
     let (count, set_count) = create_signal(cx, 0);
 
     view! { cx,
-        <button
+        <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
             on:click=move |_| {
                 set_count.update(|n| *n += if *n == 0 { 42 } else { 1 });
             }
