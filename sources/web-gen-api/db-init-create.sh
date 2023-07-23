43a3c3cff93320511be8dadcfd26f196e112435d
Drewry Pope <drewrypope@gmail.com>
2023-07-23 04:33:58 -0500

    postgres

--- sources/web-gen-api/db-init-create.sh
+++ sources/web-gen-api/db-init-create.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 initdb -D .tmp/db/postgres
-pg_ctl -D .tmp/db/postgres -l logfile -o "--unix_socket_directories='$PWD/.tmp/unix_socket_directories'" start
+pg_ctl -D .tmp/db/postgres -l logfile -o "--unix_socket_directories='$PWD/.tmp/db/unix_socket_directories'" start
 createdb -h "$(pwd)/.tmp/db/unix_socket_directories" db
