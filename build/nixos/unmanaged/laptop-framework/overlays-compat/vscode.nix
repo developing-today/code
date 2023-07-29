b1f156354b5fd6b7ea1ba867feb170db8e659039
Drewry Pope <drewrypope@gmail.com>
2023-07-28 21:45:16 -0500

    insid

--- build/nixos/unmanaged/laptop-framework/overlays-compat/vscode.nix
+++ build/nixos/unmanaged/laptop-framework/overlays-compat/vscode.nix
@@ -5,7 +5,7 @@ let
   };
 in
 {
-  vscodedasdasdas = vscode-insider.overrideAttrs (oldAttrs: rec {
+  vscodedasdasdas = vscode-xadasdinsider.overrideAttrs (oldAttrs: rec {
     src = (builtisadasdns.fetchTarball {
       url = "https://upasddate.code.visualstudio.com/latest/linux-x64/insider";
       sha256 = "03nmmcr8caasdnxnhxpsd2d5rfqi6d7njab4c3bpcqmfi9xbk3scx1a";
