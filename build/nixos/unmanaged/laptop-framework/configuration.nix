9d3c1453fcdc57d68e29e641657aee34d2a1b47a
Drewry Pope <drewrypope@gmail.com>
2023-07-28 21:49:34 -0500

    insid

--- build/nixos/unmanaged/laptop-framework/configuration.nix
+++ build/nixos/unmanaged/laptop-framework/configuration.nix
@@ -15,7 +15,7 @@
       vscode = vscode-insider.overrideAttrs (oldAttrs: rec {
         src = (builtins.fetchTarball {
           url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
-          sha256 = "03nmmcr8canxnhxpsd2d5rfqi6d7njab4c3bpcqmfi9xbk3scx1a";
+          sha256 = "16fzxqs6ql4p2apq9aw7l10h4ag1r7jwlfvknk5rd2zmkscwhn6z";
         });
         version = "latest";
       });
