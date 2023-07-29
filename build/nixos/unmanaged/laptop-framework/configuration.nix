c8fd522f3f3ca7b706fd9c95e33c28b045e3f99d
Drewry Pope <drewrypope@gmail.com>
2023-07-28 22:05:53 -0500

    insid

--- build/nixos/unmanaged/laptop-framework/configuration.nix
+++ build/nixos/unmanaged/laptop-framework/configuration.nix
@@ -1,51 +1,21 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, options, ... }:
-let
-  vscode-insiders = pkgs.stdenv.mkDerivation rec {
-    pname = "vscode-insiders";
-    version = "latest";
 
-    src = pkgs.fetchurl {
-      url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
-      sha256 = "9dd143e87499eac31382cdd5feeecde1d06debfe791a9b070e8a357ced0a81f5"; # You will need to update this
-    };
-
-    nativeBuildInputs = [ pkgs.makeWrapper ];
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp -r $src $out/vscode-insiders
-      ln -s $out/vscode-insiders/code-insiders $out/bin/code-insiders
-    '';
-
-    meta = with pkgs.lib; {
-      description = "Visual Studio Code - Insiders";
-      homepage = "https://code.visualstudio.com/insiders/";
-      license = licenses.mit;
-      platforms = platforms.linux;
-    };
-  };
-in
 {
-  #  nixpkgs.overlays = [ (final: prev:
-  #  let
-  #     vscode-insider = prev.vscode.override {
-  #       isInsiders = true;
-  #     };
-  #   in
-  #   {
-  #     vscode = vscode-insider ; #.overrideAttrs (oldAttrs: rec {
-  #       #src = (builtins.fetchTarball {
-  #         #url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
-  #         #sha256 = "16fzxqs6ql4p2apq9aw7l10h4ag1r7jwlfvknk5rd2zmkscwhn6z";
-  #       #});
-  #       #version = "latest";
-  #     #});
-  #   }
-  #  ) ];
+  nixpkgs.overlays = [ (final: prev:
+    let
+      vscode-insider = prev.vscode.override {
+        isInsiders = true;
+        src = builtins.fetchTarball {
+          url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
+          sha256 = "16fzxqs6ql4p2apq9aw7l10h4ag1r7jwlfvknk5rd2zmkscwhn6z";
+        };
+        version = "latest";
+      };
+    in
+    {
+      vscode = vscode-insider;
+    }
+  ) ];
 
   imports =
     [ # Include the results of the hardware scan.
@@ -53,20 +23,15 @@ in
       ./cachix.nix
     ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Chicago";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -79,23 +44,17 @@ in
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
-  # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
   };
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -104,18 +63,8 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.user = {
     isNormalUser = true;
     description = "user";
@@ -123,57 +72,26 @@ in
     packages = with pkgs; [
       firefox
       kate
-    #  thunderbird
     ];
   };
 
-  # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "user";
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-	git
-	vscode
-	vscode-insiders
+    git
+    vscode
+    vscode-insiders
   ];
 
   programs.neovim = {
     enable = true;
     defaultEditor = true;
   };
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.05";
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
