f7bf7dc4c011b28f91129c53dc35fa6686de4b0f
Drewry Pope <drewrypope@gmail.com>
2023-08-05 06:29:17 -0500

    ⚜⩣🥧 smile 2023-08-05

--- lib.sh
+++ lib.sh
@@ -562,12 +562,16 @@ datetime() {
   printf "%s" "$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
 }
 
+today() {
+  printf "%s" "$(date -u +"%Y-%m-%d")"
+}
+
 # shellcheck disable=SC2059,SC2317
 git_commit_all() {
   local commit_message random_emojis random_emoji_name
   random_emojis="$(random_emoji)$(random_emoji)$(random_emoji)"
   random_emoji_name=$(random_emoji_name || random_word)
-  commit_message=${1:-"${random_emojis} ${random_emoji_name} $(datetime)"}
+  commit_message=${1:-"${random_emojis} ${random_emoji_name} $(today)"}
 
   git add -A
   git commit -m "${commit_message}"
