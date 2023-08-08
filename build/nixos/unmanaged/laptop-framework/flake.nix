45d193d76548898ea9e67c0aaae02da32e629793
Drewry Pope <drewrypope@gmail.com>
2023-08-07 20:50:07 -0500

    𡲛❔🉑lets try zig flake

--- build/nixos/unmanaged/laptop-framework/flake.nix
+++ build/nixos/unmanaged/laptop-framework/flake.nix
@@ -1,15 +1,12 @@
 {
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.zig.url = "github:mitchellh/zig-overlay";
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, zig, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-
-        # add things here
       ];
     };
   };
