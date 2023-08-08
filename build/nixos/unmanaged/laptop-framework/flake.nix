a2d45aaddbe65e563358c612420abfbfc2ebd5ae
Drewry Pope <drewrypope@gmail.com>
2023-08-07 21:13:01 -0500

    ▫⭍⤴flake again

--- build/nixos/unmanaged/laptop-framework/flake.nix
+++ build/nixos/unmanaged/laptop-framework/flake.nix
@@ -1,11 +1,15 @@
 {
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.zig-overlay.url = "mitchellh/zig-overlay";
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, zig-overlay, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        { config, ... }: {
+          nixpkgs.overlays = [ zig-overlay.defaultPackage ];
+        }
       ];
     };
   };
