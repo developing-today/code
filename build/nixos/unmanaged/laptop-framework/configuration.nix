acdda04b492168f7030c8764559674b4479fab13
drewry pope <drewrypope@gmail.com>
2023-08-09 22:28:23 -0500

    🖖𡄯😗nicolas.files

--- build/nixos/unmanaged/laptop-framework/configuration.nix
+++ build/nixos/unmanaged/laptop-framework/configuration.nix
@@ -119,6 +119,41 @@ in
     # mlocate # shadowed by plocate
     universal-ctags # for nvim nvchad custom
     neofetch # sysinfo
+      awscli
+  jq
+  fd
+  htop
+  tree
+  curl
+  wget
+  helix
+  pstree
+  mkcert
+  gitAndTools.diff-so-fancy
+  ripgrep
+  fortune
+  lazygit
+  thefuck
+  tree-sitter
+  gnupg
+  gnused
+  silver-searcher
+  fzf
+  less
+  git
+  git-absorb
+  tmux
+  tmuxPlugins.continuum
+  tmuxPlugins.resurrect
+  openconnect
+  jetbrains-mono
+  sops
+  k9s
+  ## block ick
+  # These are so intellij file watchers has something to use
+  nodejs-18_x
+  nodePackages.prettier
+  ## endblock ick
   ];
 
   programs.neovim = {
