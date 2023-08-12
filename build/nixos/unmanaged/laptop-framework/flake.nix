161492de5df645328e08edd28d9fa5793323ab72
drewry pope <drewrypope@gmail.com>
2023-08-12 07:01:41 -0500

    😹㞇䘈merman

--- build/nixos/unmanaged/laptop-framework/flake.nix
+++ build/nixos/unmanaged/laptop-framework/flake.nix
@@ -8,84 +8,51 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
   outputs = { self, nixpkgs, zig-overlay, flake-utils, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      stateVersion = "23.11";
-
-      homeManagerConfiguration = { pkgs, ... }: {
-        imports = [
-          home-manager.nixosModules.home-manager
-        ];
-
-        home-manager.users.user = {
-          home.stateVersion = stateVersion;
-
-          programs.neovim = {
-            enable = true;
-            defaultEditor = true;
-            viAlias = true;
-            vimAlias = true;
-
-            plugins = [
-              pkgs.vimPlugins.nvim-tree-lua
-              {
-                plugin = pkgs.vimPlugins.sqlite-lua;
-                config = "let g:sqlite_clib_path = '${pkgs.sqlite.out}/lib/libsqlite3.so'";
-              }
-              {
-                plugin = pkgs.vimPlugins.vim-startify;
-                config = "let g:startify_change_to_vcs_root = 0";
-              }
-              pkgs.vimPlugins.vim-nix
-            ];
-          };
-
-          activation.createSymlink = ''
-            checkAndDeleteIfEmpty() {
-              if [ -d "$1" ] && [ -z "$(find "$1" -maxdepth 0 -empty)" ]; then
-                rm -r "$1"
-              fi
-            }
-
-            checkAndDeleteIfEmpty "~/.config/nvim"
-            checkAndDeleteIfEmpty "~/NvChad"
-            checkAndDeleteIfEmpty "~/forks/NvChad"
-            checkAndDeleteIfEmpty "~/NvChad/lua/custom"
-
-            if [ ! -d "~/.config/nvim" ]; then
-              if [ -d "~/NvChad" ] || [ -d "~/forks/NvChad" ]; then
-                ln -sf "~/NvChad" "~/.config/nvim" || ln -sf "~/forks/NvChad" "~/.config/nvim"
-              else
-                git clone --depth 1 "https://github.com/developing-today-forks/NvChad" "~/NvChad"
-              fi
-
-              if [ ! -d "~/NvChad/lua/custom" ]; then
-                git clone --depth 1 "https://github.com/developing-today-forks/NvChad-custom" "~/NvChad-custom"
-                ln -sf "~/NvChad-custom" "~/NvChad/lua/custom"
-              fi
-            fi
-          '';
-        };
-      };
-
-    in
-    {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./configuration.nix
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    stateVersion = "23.11";
+  homeManagerConfiguration = { pkgs, ... }: {
+    imports = [
+      home-manager.nixosModules.home-manager
+    ];
+    home-manager.users.user = {
+      home.stateVersion = stateVersion;
+      programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+        viAlias = true;
+        vimAlias = true;
+        plugins = [
+          pkgs.vimPlugins.nvim-tree-lua
           {
-            nixpkgs.overlays = [
-              (final: prev: { zigpkgs = zig-overlay.packages.${prev.system}; })
-            ];
-            system.stateVersion = stateVersion;
+            plugin = pkgs.vimPlugins.sqlite-lua;
+            config = "let g:sqlite_clib_path = '${pkgs.sqlite.out}/lib/libsqlite3.so'";
           }
-          homeManagerConfiguration
+          {
+            plugin = pkgs.vimPlugins.vim-startify;
+            config = "let g:startify_change_to_vcs_root = 0";
+          }
+          pkgs.vimPlugins.vim-nix
         ];
       };
     };
+  };
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./configuration.nix
+        {
+          nixpkgs.overlays = [
+            (final: prev: { zigpkgs = zig-overlay.packages.${prev.system}; })
+          ];
+          system.stateVersion = stateVersion;
+        }
+        homeManagerConfiguration
+      ];
+    };
+  };
 }
 
