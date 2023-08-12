2ae88d96404e549c2a71325bd267ae7ad819b1d2
drewry pope <drewrypope@gmail.com>
2023-08-12 08:11:10 -0500

    ✍🛑㬢israel

--- build/nixos/unmanaged/laptop-framework/flake.nix
+++ build/nixos/unmanaged/laptop-framework/flake.nix
@@ -25,7 +25,7 @@
         viAlias = true;
         vimAlias = true;
         vimdiffAlias = true;
-        extraConfig = "source _init.lua;";
+        extraConfig = "source _init.lua";
         plugins = [
           pkgs.vimPlugins.nvim-tree-lua
           {
