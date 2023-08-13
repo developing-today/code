4dae6022fd526a78e72056107f483b4ed1cf1449
drewry pope <drewrypope@gmail.com>
2023-08-13 05:40:48 -0500

    ⾜🎗🪐dizzy

--- build/nixos/unmanaged/laptop-framework/flake.nix
+++ build/nixos/unmanaged/laptop-framework/flake.nix
@@ -2,14 +2,14 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     zig-overlay.url = "github:mitchellh/zig-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, zig-overlay, flake-utils, home-manager, neovim-nightly-overlay, ... }:
+  outputs = { self, nixpkgs, zig-overlay, neovim-nightly-overlay, flake-utils, home-manager, ... }:
   let
     system = "x86_64-linux";
     overlay = neovim-nightly-overlay.overlay;
@@ -27,8 +27,24 @@
           viAlias = true;
           vimAlias = true;
           vimdiffAlias = true;
-          package = pkgs.neovim-nightly; # Use the nightly package
-          # Rest of the neovim configuration
+          package = pkgs.neovim-nightly;
+          extraConfig = ''
+            set runtimepath+=/home/user/forks/NvChad
+            set packpath+=/home/user/forks/NvChad
+            luafile /home/user/forks/NvChad/_init.lua
+          '';
+          plugins = [
+            pkgs.vimPlugins.nvim-tree-lua
+            {
+              plugin = pkgs.vimPlugins.sqlite-lua;
+              config = "let g:sqlite_clib_path = '${pkgs.sqlite.out}/lib/libsqlite3.so'";
+            }
+            {
+              plugin = pkgs.vimPlugins.vim-startify;
+              config = "let g:startify_change_to_vcs_root = 0";
+            }
+            pkgs.vimPlugins.vim-nix
+          ];
         };
       };
     };
@@ -40,7 +56,7 @@
         {
           nixpkgs.overlays = [
             (final: prev: { zigpkgs = zig-overlay.packages.${prev.system}; })
-            overlay # Include the Neovim nightly overlay
+            overlay
           ];
           system.stateVersion = stateVersion;
         }
