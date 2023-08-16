48e45eb676e55211d72790e085bf469777b5413f
Drewry Pope <drewrypope@gmail.com>
2023-08-16 05:39:56 -0500

    Update configuration.nix
    Signed-off-by: Drewry Pope <drewrypope@gmail.com>

--- build/nixos/unmanaged/laptop-framework/configuration.nix
+++ build/nixos/unmanaged/laptop-framework/configuration.nix
@@ -212,6 +212,40 @@ gcc
     fortune
     figlet
     lolcat
+
+        tree-sitter
+        nodejs
+        # Language Servers
+        # Bash
+        nodePackages.bash-language-server
+        # Dart
+        dart
+        # Elixir
+        beam.packages.erlang.elixir-ls
+        # Erlang
+        beam.packages.erlang.erlang-ls
+        # Haskell
+        stable.haskellPackages.haskell-language-server
+        # Lua
+        lua-language-server
+        # Nix
+        nil
+        nixpkgs-fmt
+        statix
+        # Python
+        pyright
+        python-debug
+        black
+        # Typescript
+        nodePackages.typescript-language-server
+        # Web (ESLint, HTML, CSS, JSON)
+        nodePackages.vscode-langservers-extracted
+        # Telescope tools
+        ripgrep
+        fd
+
+    nickel
+
   ];
 
   programs.neovim = {
