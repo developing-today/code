30e855ca0ccd8a42d4d93a09df22d0cbfda84191
Drewry Pope <drewrypope@gmail.com>
2023-12-17 19:05:36 -0600

    fixup

--- sources/data/resources/general/migrations/2023/006_adduuid.dtm.sql
+++ sources/data/resources/general/migrations/2023/006_adduuid.dtm.sql
@@ -2,7 +2,7 @@
 ALTER TABLE nodes
     DROP CONSTRAINT IF EXISTS nodes_team_id_fkey, -- Drop existing foreign key constraint
     ADD COLUMN IF NOT EXISTS team_id UUID,       -- Add the column if it doesn't exist
-    ADD CONSTRAINT nodes_team_id_fkey FOREIGN KEY (team_id) REFERENCES teams(id); -- Add foreign key constraint
+    ADD CONSTRAINT nodes_team_id_fkey FOREIGN KEY (team_id) REFERENCES nodes(id); -- Add foreign key constraint
 
 -- +goose Down
 ALTER TABLE nodes
