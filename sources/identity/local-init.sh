dcc30e54ebf99f24d5d80bad3ee4f034b7df42f4
Drewry Pope <drewrypope@gmail.com>
2024-03-18 21:39:22 -0500

    local

---
+++ sources/identity/local-init.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo CHARM_LINK_URL=http://localhost:3333/link NO_INSTALL=1 ./init.template.sh
---
+++ sources/identity/local-install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo CHARM_LINK_URL=http://localhost:3333/link ./init.template.sh
