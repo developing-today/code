b65eb66ed21f0a387d9ada4a465c9db57dd4e19f
Drewry Pope <drewrypope@gmail.com>
2024-04-02 02:35:27 -0500

    depl

---
+++ sources/identity/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+function deploy() {
+  HOST="${1:-localhost}"
+  PORT="${2:-3333}"
+  PROTOCOL="${3:-http}"
+  PATH="${4:-link}"
+  cd ~
+  rm -rf code
+  git clone https://github.com/developing-today/code
+  cd code/sources/identity
+  chmod -R +x *.sh
+  CHARM_LINK_URL="${PROTO}://${HOST}:${PORT}/${PATH}" NO_INSTALL=1 ./init.template.sh
+}
+if [ "$#" -gt 0 ]; then
+  deploy $@
+fi
