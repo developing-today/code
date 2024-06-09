b8d1e2f174cd25e5a7239fce2d2307afc3064896
Drewry Pope <drewrypope@gmail.com>
2024-06-08 20:41:18 -0500

    feat: docker not podman

--- start-xenorchestra.ps1
+++ start-xenorchestra.ps1
@@ -3,7 +3,8 @@
 New-Item -ItemType Directory -Force -Path "$PSScriptRoot\data\xo-server"
 New-Item -ItemType Directory -Force -Path "$PSScriptRoot\data\redis"
 
-podman run -itd `
+#podman run -itd `
+docker run -itd `
   --stop-timeout 60 `
   --restart unless-stopped `
   --cap-add sys_admin `
