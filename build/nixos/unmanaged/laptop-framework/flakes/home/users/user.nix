8f66cb19b6170874226addfa178a483dc1ad435a
Drewry Pope <drewrypope@gmail.com>
2024-07-07 02:13:30 -0500

    chore: nixos

--- build/nixos/unmanaged/laptop-framework/flakes/home/users/user.nix
+++ build/nixos/unmanaged/laptop-framework/flakes/home/users/user.nix
@@ -874,9 +874,6 @@
       };
       extraConfig = ''
         $env.config = {
-            filesize_metric: false
-            table_mode: rounded
-            use_ls_colors: true
            show_banner: false,
         }
       '';
