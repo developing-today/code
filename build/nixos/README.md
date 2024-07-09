653a0ef3eb9b4494acf4515d54d8898e5fcdeeda
Drewry Pope <drewrypope@gmail.com>
2024-07-08 21:57:37 -0500

    refactor: nixos

--- build/nixos/README.md
+++
@@ -1,9 +0,0 @@
-<div align="center">
-  <img src="logo.svg" width="250" />
-  <h1>The Hive</h1>
-  <p>The secretly open NixOS-Society</span>
-</div>
-
----
-
-<sub>It is forbidden to modify this readme &rarr; read the code!</sub>
--- build/nixos/flake.nix
+++ build/nixos/flake.nix
@@ -1,115 +1,145 @@
 {
-  description = "The Hive - The secretly open NixOS-Society";
-  inputs.std.url = "github:divnix/std";
-  inputs.std.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.std.inputs.mdbook-kroki-preprocessor.follows = "std/blank";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  #github:eza-community/eza
-  # tools
   inputs = {
-    nixos-generators.url = "github:blaggacao/nixos-generators";
-    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-generators.inputs.nixlib.follows = "nixpkgs";
-    colmena.url = "github:zhaofengli/colmena";
-    colmena.inputs.nixpkgs.follows = "nixpkgs";
-    colmena.inputs.stable.follows = "std/blank";
-    colmena.inputs.flake-utils.follows = "std/flake-utils";
-    nixgl.url = "github:guibou/nixGL";
-    nixgl.inputs.nixpkgs.follows = "nixpkgs";
-    nixgl.inputs.flake-utils.follows = "std/flake-utils";
-    disko.url = "github:blaggacao/disko/module";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  # nixpkgs & home-manager
-  inputs = {
-    nixos.follows = "nixos-22-05";
-    home.follows = "home-22-05";
-
-    nixos-22-05.url = "github:nixos/nixpkgs/release-22.05";
-    home-22-05.url = "github:blaggacao/home-manager/release-22.05"; # some temp fixes
-    nixos-21-11.url = "github:nixos/nixpkgs/release-21.11";
-    home-21-11.url = "github:blaggacao/home-manager/release-21.11-with-nix-profile";
-  };
-
-  outputs = {
-    std,
-    self,
-    ...
-  } @ inputs:
-    std.growOn {
-      inherit inputs;
-      cellsFrom = ./comb;
-      # debug = ["cells" "x86_64-linux"];
-      cellBlocks = with std.blockTypes; [
-        # modules implement
-        (functions "nixosModules")
-        (functions "homeModules")
-        (functions "devshellModules")
-
-        # profiles activate
-        (functions "hardwareProfiles")
-        (functions "nixosProfiles")
-        (functions "homeProfiles")
-        (functions "devshellProfiles")
-
-        # suites aggregate profiles
-        (functions "nixosSuites")
-        (functions "homeSuites")
-
-        # configurations can be deployed
-        (data "nixosConfigurations")
-        (data "colmenaConfigurations")
-        (data "homeConfigurations")
-
-        # devshells can be entered
-        (devshells "devshells")
-
-        # jobs can be run
-        (runnables "jobs")
-
-        # library holds shared knowledge made code
-        (functions "library")
-      ];
-      nixpkgsConfig = {
-        allowUnfree = true;
-      };
-    }
-    # soil
-    {
-      # tool: colmena -- "fill the jar on the soil with the honey!"
-      colmenaHive = let
-        makeHoneyFrom = import ./make-honey.nix {
-          inherit (inputs) colmena nixpkgs;
-          cellBlock = "colmenaConfigurations";
-        };
-      in
-        makeHoneyFrom self;
-
-      # tool: nixos-generators -- "get drunk like a bear!"
-      nixosConfigurations = let
-        makeMeadFrom = import ./make-mead.nix {
-          inherit (inputs) nixpkgs;
-          cellBlock = "nixosConfigurations";
-        };
-      in
-        makeMeadFrom self;
-
-      # tool: home-manager -- "drunken sailor, sunken sailor; honeymoon pantaloon."
-      homeConfigurations = let
-        makeMoonshineFrom = import ./make-moonshine.nix {
-          inherit (inputs) nixpkgs;
-          cellBlock = "homeConfigurations";
-        };
-      in
-        makeMoonshineFrom self;
+    #nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:dezren39/nixpkgs/master";
+    #nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2305.491756.tar.gz"; # /nixos-23.11";
+    #nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz"; # /nixos-unstable"; # /nixos-23.11";
+    #nixpkgs.url = "github:DeterminateSystems/nixpkgs/nix_2_18_1";
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      #inputs.nixpkgs-stable.follows ="nixpkgs";
+      #inputs.nixpkgs.follows ="nixpkgs";
     };
 
-  # --- Flake Local Nix Configuration ----------------------------
-  # TODO: adopt spongix
-  nixConfig = {
-    extra-substituters = [];
-    extra-trusted-public-keys = [];
+    home = {
+      url = "path:./flakes/home";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.neovim-nightly-overlay.follows = "neovim-nightly-overlay";
+      inputs.hercules-ci-effects.follows = "hercules-ci-effects";
+      inputs.home-manager.follows = "home-manager";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager"; # */
+      #url = "https://flakehub.com/f/nix-community/home-manager/*.tar.gz"; #*/
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    #  hardware.url = "github:nixos/nixos-hardware"; # todo figure out how to use this
+    flake-utils.url = "https://flakehub.com/f/numtide/flake-utils/*.tar.gz"; # */ # inputs.systems
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.0.1.tar.gz";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    hercules-ci-effects = {
+      url = "github:hercules-ci/hercules-ci-effects";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.hercules-ci-effects.follows = "hercules-ci-effects";
+      inputs.flake-compat.follows = "flake-compat";
+    };
+    # must type follows all out every time
+    # because flake inputs are basically static
+    # can't make a let var function closure thing around it or whatever
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay"; # url = "github:developing-today-forks/zig-overlay/quote-urls";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    alejandra = {
+      url = "https://flakehub.com/f/kamadorueda/alejandra/*.tar.gz"; # */ # url = "github:developing-today-forks/alejandra/quote-urls";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flakeCompat.follows = "flake-compat";
+    };
+    #nix-software-center = {
+    #  url = "github:snowfallorg/nix-software-center"; #https://flakehub.com/f/vlinkz/nix-software-center/0.1.2.tar.gz";
+    #  #url = "github:vlinkz/nix-software-center"; #https://flakehub.com/f/vlinkz/nix-software-center/0.1.2.tar.gz";
+    #  #url = "github:vlinkz/nix-software-center"; #https://flakehub.com/f/vlinkz/nix-software-center/0.1.2.tar.gz";
+    #  #url = "https://flakehub.com/f/vlinkz/nix-software-center/*.tar.gz"; #*/ # https://github.com/vlinkz/nix-software-center/pull/50
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #  inputs.utils.follows = "flake-utils";
+    #};
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      #url = "github:developing-today-forks/nix-community_nixvim";
+      #url = "github:developing-today-forks/nixvim-flake";
+      #inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # nix-rice = https://github.com/bertof/nix-rice # todo fork and rename this garbage
   };
-  # --------------------------------------------------------------
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      home,
+      zig-overlay,
+      alejandra,
+      sops-nix,
+      #nix-software-center,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+      yes = "yes";
+      stateVersion = "23.11";
+      #stateVersion = "23.05";
+      overlays = [
+        zig-overlay.overlays.default
+        alejandra.overlay
+        #nix-software-center.overlay
+        home.vim-overlay
+      ];
+      systemNixosModules = [
+        {
+          nixpkgs = {
+            overlays = overlays; # are overlays needed in home manager? document which/why?
+            config = {
+              allowUnfree = true;
+              permittedInsecurePackages = [
+                "electron" # le sigh
+              ];
+            };
+          };
+          system.stateVersion = stateVersion;
+        }
+        ./modules/configuration.nix # this relies on magic overlays, ? todo: remove overlays from configuration.nix? then add inline let overlay configuration right here below this moduleArrayList.
+        #sops-nix.nixosModules.sops
+      ];
+      # overlayNixosModules = ?
+      hyprlandNixosModules = [
+        (import ./modules/hyprland.nix) # hyprland = would use flake for hyprland master but had annoying warning about waybar? todo try again. prefer flake. the config for this is setup in homeManager for reasons. could be brought out to nixos module would probably fit better due to my agonies
+        #       (import ./modules/nm-applet.nix)
+      ];
+      homeManagerNixosModules = home.homeManagerNixosModules stateVersion;
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        system = system;
+        overlays = overlays;
+      };
+      lib = nixpkgs.lib; # // home-manager.lib;
+    in
+    {
+      inherit lib;
+      #hydraJobs = import ./modules/hydra.nix { inherit inputs outputs; }; # https://git.sr.ht/~fd/nix-configs/tree/main/item/flake.nix
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = systemNixosModules ++ hyprlandNixosModules ++ homeManagerNixosModules;
+      };
+      specialArgs = {
+        inherit inputs outputs;
+      };
+    };
+  # hydra
+  # content addressible
 }
