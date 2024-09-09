a0bce86b00bb843d2d119bc571840f8556b4307e
Drewry Pope <drewrypope@gmail.com>
2024-09-08 20:56:16 -0500

    chore(nix): update

--- lib/rebuild.sh
+++ lib/rebuild.sh
@@ -55,6 +55,13 @@ if [[ -f "./flake.nix" ]]; then
   nix flake update --print-build-logs --verbose --keep-going --log-format internal-json --fallback --show-trace |& nom --json
   echo "git add ."
   git add .
+  if systemctl is-active --quiet tailscaled; then # this is a hack: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-2134547782
+    echo "stopping tailscaled..." # this is a hack: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-2134547782
+    sudo systemctl stop tailscaled # this is a hack: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-2134547782
+    echo "tailscaled service stopped." # this is a hack: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-2134547782
+  else
+    echo "tailscaled service not found or not active." # hack not needed
+  fi
   echo "running nixos-rebuild switch..."
   sudo nixos-rebuild --accept-flake-config --json switch --json --upgrade --json --print-build-logs --verbose --keep-going --log-format internal-json --fallback --show-trace --flake '.' |& nom --json
   #else
--- lib/simple-rebuild.sh
+++ lib/simple-rebuild.sh
@@ -13,4 +13,11 @@ echo "entered: $script_dir"
 echo "git add ."
 
 git add .
+if systemctl is-active --quiet tailscaled; then # this is a hack: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-2134547782
+  echo "stopping tailscaled..." # this is a hack: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-2134547782
+  sudo systemctl stop tailscaled # this is a hack: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-2134547782
+  echo "tailscaled service stopped." # this is a hack: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-2134547782
+else
+  echo "tailscaled service not found or not active." # hack not needed
+fi
 sudo nixos-rebuild --accept-flake-config --json switch --json --upgrade --json --print-build-logs --verbose --keep-going --log-format internal-json --fallback --show-trace --flake '.' |& nom --json
