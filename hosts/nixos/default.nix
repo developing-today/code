b88bd32485a7393367a7cc5931fc69926ec17ce3
Drewry Pope <drewrypope@gmail.com>
2024-09-14 03:49:49 -0500

    254 current  2024-09-14 03:49:38  24.11.20240909.9f82045  6.6.50                          *

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -1,13 +1,16 @@
 { inputs, outputs, lib, pkgs, ... }:
+let
+  system = "x86_64-linux";
+  stateVersion = "23.11";
+  overlays = outputs.overlays.${system};
+in
 {
   nixos = lib.nixosSystem {
     modules = lib.lists.flatten [
-      (import ../common/modules/desktop { inherit inputs outputs lib pkgs;
-        stateVersion = "23.11";
-        system = "x86_64-linux";
-      })
+      (import ../common/modules/desktop { inherit inputs outputs lib pkgs system stateVersion; })
       ../common/modules/hardware-configuration/framework-13/intel
     ];
+    # overlays = overlays;
     specialArgs = {
       inherit inputs outputs lib pkgs;
     };
