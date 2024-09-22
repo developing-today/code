7016023230882f3d8f33a3bfd1d921d707947b94
Drewry Pope <drewrypope@gmail.com>
2024-09-22 07:35:45 -0500

    296 current  2024-09-22 07:35:33  24.11.20240918.5f145b0  6.6.51                          *

--- flake.nix
+++ flake.nix
@@ -10,8 +10,8 @@
       systems = [ "x86_64-linux" ];
       flake =
         let
-          lib = import ./lib { inherit self; lib = (inputs.nixpkgs.lib.attrsets.recursiveUpdate
-            inputs.nixpkgs.lib inputs.home-manager.lib); };
+          lib = import ./lib { inherit self; lib = inputs.nixpkgs.lib.attrsets.recursiveUpdate
+            inputs.nixpkgs.lib inputs.home-manager.lib; };
         in
         # lib.attrsets.recursiveUpdate # todo recursiveUpdate but lists append/dedupe
         import ./hosts {
