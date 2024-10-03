57a4572c3c09796604dee514b3c6ceffefeb412a
Drewry Pope <drewrypope@gmail.com>
2024-10-02 23:20:58 -0500

    315 current  2024-10-02 23:20:45  24.11.20240918.5f145b0  6.6.51                          *

--- hosts/default.nix
+++ hosts/default.nix
@@ -50,7 +50,8 @@
           # TODO: ./configurations/${hostname}.nix
           # Desktop configuration
           # TODO: profiles to select in struct, this gets imported
-          (lib.make-profile-paths { basePath = "hosts/common/modules"; } [ "desktop" ])
+          ((lib.make-profile-paths {}) "desktop")
+          # (lib.make-profile-paths { basePath = "hosts/common/modules"; } [ "desktop" ])
           # (lib.from-root "hosts/common/modules/desktop")
         ];
       }
