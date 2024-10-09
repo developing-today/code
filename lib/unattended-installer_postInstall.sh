5ceb46dac7363a785095cd4097292d319d47332d
Drewry Pope <drewrypope@gmail.com>
2024-10-08 23:02:38 -0500

    stuff

--- lib/unattended-installer_postInstall.sh
+++ lib/unattended-installer_postInstall.sh
@@ -2,21 +2,4 @@
 set -e #-o pipefail
 
 echo "postInstall starting"
-echo "Copying /iso/bootstrap to /mnt/bootstrap..."
-cp -r /iso/bootstrap /mnt
-echo "Done copying /iso/bootstrap to /mnt/bootstrap"
-
-echo "Listing /mnt/bootstrap..."
-ls -lahR /mnt/bootstrap
-echo "Done listing /mnt/bootstrap"
-
-echo "Uncompressing all .tar.gz files in /mnt/bootstrap..."
-find /mnt/bootstrap -name "*.tar.gz" -exec sh -c '
-    dir=$(dirname "$1")
-    base=$(basename "$1" .tar.gz)
-    mkdir -p "$dir/$base"
-    tar -xzf "$1" -C "$dir/$base"
-' sh {} \;
-echo "Done uncompressing all .tar.gz files in /mnt/bootstrap"
-
 echo "postInstall done"
--- lib/unattended-installer_preInstall.sh
+++ lib/unattended-installer_preInstall.sh
@@ -2,4 +2,21 @@
 set -e #-o pipefail
 
 echo "preInstall starting"
+echo "Copying /iso/bootstrap to /mnt/bootstrap..."
+cp -r /iso/bootstrap /mnt
+echo "Done copying /iso/bootstrap to /mnt/bootstrap"
+
+echo "Listing /mnt/bootstrap..."
+ls -lahR /mnt/bootstrap
+echo "Done listing /mnt/bootstrap"
+
+echo "Uncompressing all .tar.gz files in /mnt/bootstrap..."
+find /mnt/bootstrap -name "*.tar.gz" -exec sh -c '
+    dir=$(dirname "$1")
+    base=$(basename "$1" .tar.gz)
+    mkdir -p "$dir/$base"
+    tar -xzf "$1" -C "$dir/$base"
+' sh {} \;
+echo "Done uncompressing all .tar.gz files in /mnt/bootstrap"
+
 echo "preInstall done"
