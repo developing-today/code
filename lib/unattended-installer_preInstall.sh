e1de5e0a320e9bb80c8552b54dbc41a37cd33c02
Drewry Pope <drewrypope@gmail.com>
2024-10-13 06:12:52 -0500

    fix

--- lib/unattended-installer_preInstall.sh
+++ lib/unattended-installer_preInstall.sh
@@ -80,7 +80,7 @@ echo "/mnt/nix/persistent/home/user/.config created" # TODO: auto-create all use
 echo "chown -R 1337:100 /mnt/nix/persistent/home/user"
 echo "TODO: handle this better, automatically for all users"
 echo "      this assumes 1337 is 'user' and 100 is 'users'"
-chown -R 1000:100 /mnt/nix/persistent/home/user # TODO: handle this better, automatically for all users; this assumes 1000 is 'user' and 100 is 'users'
+chown -R 1337:100 /mnt/nix/persistent/home/user # TODO: handle this better, automatically for all users; this assumes 1000 is 'user' and 100 is 'users'
 echo "TODO: handle this better, automatically for all users"
 echo "      this assumes 1337 is 'user' and 100 is 'users'"
 echo "Done chown -R 1337:100 /mnt/nix/persistent/home/user"
