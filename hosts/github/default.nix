106f72704e7b758d2d4e2e67dd3d215c8f6a413a
Drewry Pope <drewrypope@gmail.com>
2024-10-14 02:08:43 -0500

    github

--- hosts/github/default.nix
+++ hosts/github/default.nix
@@ -8,10 +8,12 @@
   lib,
   ...
 }: {
-  # sops secret
-  # sops.secrets.github-token-root = {
-  # sops.secrets.github-token-user = {
-  #   path = ""
-  #   sopsFile = lib.from-root "secrets/sops/common/tailscale.yaml";
-  # };
+  sops.secrets.github-token-root = {
+    path = "/home/user/auth";
+    sopsFile = lib.from-root "secrets/sops/groups/admin/github.yaml";
+  };
+  sops.secrets.github-token-user = {
+    path = "/root/auth";
+    sopsFile = lib.from-root "secrets/sops/groups/admin/github.yaml";
+  };
 }
