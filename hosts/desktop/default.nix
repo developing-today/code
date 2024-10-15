db986c31d6acf48891e611f85e0a4c3614a9fdcb
Drewry Pope <drewrypope@gmail.com>
2024-10-15 00:58:56 -0500

    amd 14 current  2024-10-15 00:58:52  24.11.20241014.b36c6a9  6.6.56                          *

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,18 +1,14 @@
 {
-  config,
   inputs,
-  hostName,
-  host,
-  system,
-  stateVersion,
   lib,
-  pkgs,
+  system,
   ...
 }:
 {
   imports = [
+    (lib.from-root "hosts/all")
     (lib.from-root "hosts/tailscale-autoconnect")
-    (lib.from-root "hosts/home")
+    (lib.from-root "hosts/home") # users should import home users as-needed
     inputs.vim.nixosModules.${system}
     (lib.from-root "hosts/hyprland") # hyprland = would use flake for hyprland master but had annoying warning about waybar? todo try again. prefer flake. the config for this is setup in homeManager for reasons. could be brought out to nixos module would probably fit better due to my agonies
     (lib.from-root "hosts/sops")
@@ -24,7 +20,7 @@
     (lib.from-root "hosts/i18n")
     (lib.from-root "hosts/time")
     (lib.from-root "hosts/nix")
-    (lib.from-root "hosts/users")
+    (lib.from-root "hosts/users") # users should import home users as-needed
     (lib.from-root "hosts/fonts")
     (lib.from-root "hosts/hardware")
     (lib.from-root "hosts/environment")
