e2d81450f71507a6903ff0ced7a26fda93946f0e
Drewry Pope <drewrypope@gmail.com>
2024-10-17 00:40:10 -0500

    amd 34 current  2024-10-17 00:40:04  24.11.20241014.b36c6a9  6.6.56                          *

--- hosts/printing/default.nix
+++ hosts/printing/default.nix
@@ -1,20 +1,35 @@
 { config, pkgs, ... }:
+
 {
   # Enable CUPS to print documents
   services.printing = {
     enable = true;
-    drivers = [ pkgs.gutenprint pkgs.hplip ];
+    drivers = [ pkgs.gutenprint pkgs.hplip pkgs.postscript-lexmark ];
   };
+
   # Enable avahi for network printer discovery
   services.avahi = {
     enable = true;
     nssmdns = true;
   };
+
   # Open firewall ports for CUPS
   networking.firewall.allowedTCPPorts = [ 631 ];
   networking.firewall.allowedUDPPorts = [ 631 ];
-  environment.systemPackages = with pkgs; [
-    usbutils
-    busybox
-  ];
+
+  # Add support for Dell printers
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "Dell_1815dn";
+        location = "Office";
+        deviceUri = "usb://Dell/1815dn?serial=SERIAL_NUMBER";
+        model = "drv:///sample.drv/generpcl.ppd";
+        ppdOptions = {
+          PageSize = "A4";
+        };
+      }
+    ];
+    ensureDefaultPrinter = "Dell_1815dn";
+  };
 }
