79dfd6ab1bf4b61a4f6a265d6112c9d60075ecf6
Drewry Pope <drewrypope@gmail.com>
2024-10-17 01:39:03 -0500

    amd 37 current  2024-10-17 01:38:57  24.11.20241014.b36c6a9  6.6.56                          *

--- hosts/printing/default.nix
+++ hosts/printing/default.nix
@@ -1,23 +1,20 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }: {
+  # Make the PPD file available in the Nix store
+  nixpkgs.overlays = [
+    (final: prev: {
+      dell1815dnPPD = final.runCommand "dell1815dn-ppd" {} ''
+        mkdir -p $out/share/cups/model
+        cp ${(lib.from-root "hosts/printing/mfp1815ps.ppd")} $out/share/cups/model/mfp1815ps.ppd
+      '';
+    })
+  ];
 
-{
-  # Enable CUPS to print documents
+  # Printer configuration
   services.printing = {
     enable = true;
-    drivers = [ pkgs.gutenprint pkgs.hplip pkgs.postscript-lexmark ];
+    drivers = [ pkgs.gutenprint pkgs.hplip pkgs.dell1815dnPPD ];
   };
 
-  # Enable avahi for network printer discovery
-  services.avahi = {
-    enable = true;
-    nssmdns = true;
-  };
-
-  # Open firewall ports for CUPS
-  networking.firewall.allowedTCPPorts = [ 631 ];
-  networking.firewall.allowedUDPPorts = [ 631 ];
-
-  # Add support for Dell printers
   hardware.printers = {
     ensurePrinters = [
       {
@@ -25,8 +22,13 @@
         location = "Office";
         # deviceUri = "usb://Dell/1815dn?serial=SERIAL_NUMBER";
         # direct usb://Dell/Laser%20MFP%201815?serial=CQQLCD1.........&interface=1
-        deviceUri = "usb://Dell/Laser%20MFP%201815?serial=CQQLCD1";
-        model = "drv:///sample.drv/generpcl.ppd";
+        # foomatic-db
+        # https://aur.archlinux.org/packages/dell-unified-driver
+        # https://www.bchemnet.com/suldr/supported.html
+        # openprinting
+        # https://ubuntuforums.org/archive/index.php/t-768745.html
+        deviceUri = "usb://Dell/Laser%20MFP%201815?serial=CQQLCD1&interface=1";
+        model = "mfp1815ps.ppd";
         ppdOptions = {
           PageSize = "A4";
         };
@@ -34,4 +36,12 @@
     ];
     ensureDefaultPrinter = "Dell_1815dn";
   };
+
+  # CUPS and firewall configuration
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
+  networking.firewall.allowedTCPPorts = [ 631 ];
+  networking.firewall.allowedUDPPorts = [ 631 ];
 }
