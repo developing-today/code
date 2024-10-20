fbf5162ebc2e9179d6b9174369ee937e0b8bc714
Drewry Pope <drewrypope@gmail.com>
2024-10-19 19:26:58 -0500

    amd 40 current  2024-10-19 19:26:52  24.11.20241014.b36c6a9  6.6.56                          *

--- hosts/networking/wireless/us-wi-1/default.nix
+++ hosts/networking/wireless/us-wi-1/default.nix
@@ -1,5 +1,47 @@
 {
-  "I win again, Lews Therin." = {
-    pskRaw = "ext:iwinagainlewstherin";
+  config,
+  inputs,
+  hostName,
+  host,
+  system,
+  stateVersion,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  networking = {
+    networkmanager = {
+      enable = false;
+      unmanaged = [
+        "*"
+        "except:type:wwan"
+        "except:type:gsm"
+      ];
+    };
+    wireless = {
+      enable = true;
+      # userControlled.enable = true;
+      scanOnLowSignal = true;
+      fallbackToWPA2 = true;
+      secretsFile = config.sops.secrets.wireless.path;
+      networks."I win again, Lews Therin.".pskRaw = "ext:iwinagainlewstherin";
+      allowAuxiliaryImperativeNetworks = true; # TODO: can we disable this?
+      userControlled = {
+        enable = true;
+        group = "network";
+      };
+      # whats extraConfig.update_config=1 do?
+      extraConfig = ''
+        update_config=1
+      '';
+    };
   };
+  sops.secrets."wireless" = {
+    # TODO: us-wi-1 module in hosts/networking/wireless/us-wi-1, make-wireless if wireless is not []
+    sopsFile = lib.from-root "secrets/sops/common/networking/wireless/us-wi-1.yaml";
+  };
+  # # Ensure group exists
+  # this would be for users that aren't root or sudoers or doassers or whatever
+  users.groups.network = { };
 }
