18860691063ab55d6184f403e8fe810ebe4b4fbe
Drewry Pope <drewrypope@gmail.com>
2024-10-21 18:57:56 -0500

    123

--- hosts/default.nix
+++ hosts/default.nix
@@ -21,28 +21,28 @@ in {
     users = [ "user" "backup" ];
     wireless = "us-wi-1";
   };
-  # apu2c3 = host {
-  #   profiles = ["server"];
-  #   hardware = "pcengines/apu";
-  #   disks = [
-  #     "sda1/small"
-  #     "tmpfs/root"
-  #   ];
-  #   users = [ "server" ];
-  # };
-  # apu2c4
-  # apu2c4-with-wifi
-  # apu2c3-with-modem
-  # apu2c3-with-wifi
-  # apu2c3-with-modem-and-wifi
-  # pi0
-  # pi2
-  # pi3
-  # pi4
-  # pi5
-  # fire3
-  # amd-server
-  # intel-server
-  # generic profile to connect/mount data disks
-  # generic script/process to apply a different config and use that for auto-upgrade going forward
 }
+# apu2c3 = host {
+#   profiles = ["server"];
+#   hardware = "pcengines/apu";
+#   disks = [
+#     "sda1/small"
+#     "tmpfs/root"
+#   ];
+#   users = [ "server" ];
+# };
+# apu2c4
+# apu2c4-with-wifi
+# apu2c3-with-modem
+# apu2c3-with-wifi
+# apu2c3-with-modem-and-wifi
+# pi0
+# pi2
+# pi3
+# pi4
+# pi5
+# fire3
+# amd-server
+# intel-server
+# generic profile to connect/mount data disks
+# generic script/process to apply a different config and use that for auto-upgrade going forward
