77d29a2cd6a6354b5af7cca3f47de873561b407c
Drewry Pope <drewrypope@gmail.com>
2024-10-21 18:56:05 -0500

    amd 55 current  2024-10-21 18:56:00  24.11.20241014.b36c6a9  6.6.56                          *

--- hosts/init/default.nix
+++ hosts/init/default.nix
@@ -1,56 +1,60 @@
 {
-  config,
-  inputs,
-  hostName,
   host,
-  system,
-  stateVersion,
   lib,
   ...
 }:
 {
   imports = with lib; lists.flatten [
-    /*
-      # TODO: make generic array function and use that, maybe prefix one is enough?
-      # TODO: fn to allow optionals for the auto-list below, removed before import
-      from-root "hosts/abstract" # maybe don't import all, just ones needed as needed?
-      from-root "hosts/hardware-configuration/${hostName}"
-      from-root "hosts/{host.type}"
-      from-root "hosts/{host.type}/{hostName}"
-      from-root "hosts/{host.type}/{hostName}/{profile}" for profile in host.profiles
-      from-root "hosts/{host.type}/{profile}" for profile in host.profiles
-      from-root "hosts/{host.type}/{profile}/{hostName}" for profile in host.profiles
-      from-root "hosts/{hostName}"
-      from-root "hosts/{hostName}/{host.type}"
-      from-root "hosts/{hostName}/{host.type}/{profile}" for profile in host.profiles
-      from-root "hosts/{hostName}/{profile}" for profile in host.profiles
-      from-root "hosts/{hostName}/{profile}/{host.type}" for profile in host.profiles
-      from-root "hosts/{profile}" for profile in host.profiles
-      from-root "hosts/{profile}/{host.type}" for profile in host.profiles
-      from-root "hosts/{profile}/{hostName}" for profile in host.profiles
-      from-root "hosts/{profile}/{host.type}/{hostName}" for profile in host.profiles
-      from-root "hosts/{profile}/{hostName}/{host.type}" for profile in host.profiles
-    */
     (ensure-list host.modules)
     (ensure-list host.imports)
+
     (make-hardware host.hardware)
     (ensure-list host.hardware-modules)
     (ensure-list host.hardware-imports)
-    # networking # TODO: make this work
+
     (make-profiles host.profiles)
     (ensure-list host.profile-modules)
     (ensure-list host.profile-imports)
+
     (make-disks host.disks)
     (ensure-list host.disk-modules)
     (ensure-list host.disk-imports)
+
     (make-wireless host.wireless)
-    (make-users host.users)
     (ensure-list host.wireless-modules)
     (ensure-list host.wireless-imports)
-    # (make-darwin-modules host.darwin-profiles)
+
+    (make-users host.users)
+    (ensure-list host.user-modules)
+    (ensure-list host.user-imports)
+
+    # (ensure-list host.darwin-profiles) # make-darwin-profiles
     (ensure-list host.darwin-profile-modules)
     (ensure-list host.darwin-profile-imports)
     (ensure-list host.darwin-modules)
     (ensure-list host.darwin-imports)
   ];
 }
+/*
+  # (make-darwin-modules host.darwin-profiles)
+  # networking # TODO: make this work
+  # TODO: make generic array function and use that, maybe prefix one is enough?
+  # TODO: fn to allow optionals for the auto-list below, removed before import
+  from-root "hosts/abstract" # maybe don't import all, just ones needed as needed?
+  from-root "hosts/hardware-configuration/${hostName}"
+  from-root "hosts/{host.type}"
+  from-root "hosts/{host.type}/{hostName}"
+  from-root "hosts/{host.type}/{hostName}/{profile}" for profile in host.profiles
+  from-root "hosts/{host.type}/{profile}" for profile in host.profiles
+  from-root "hosts/{host.type}/{profile}/{hostName}" for profile in host.profiles
+  from-root "hosts/{hostName}"
+  from-root "hosts/{hostName}/{host.type}"
+  from-root "hosts/{hostName}/{host.type}/{profile}" for profile in host.profiles
+  from-root "hosts/{hostName}/{profile}" for profile in host.profiles
+  from-root "hosts/{hostName}/{profile}/{host.type}" for profile in host.profiles
+  from-root "hosts/{profile}" for profile in host.profiles
+  from-root "hosts/{profile}/{host.type}" for profile in host.profiles
+  from-root "hosts/{profile}/{hostName}" for profile in host.profiles
+  from-root "hosts/{profile}/{host.type}/{hostName}" for profile in host.profiles
+  from-root "hosts/{profile}/{hostName}/{host.type}" for profile in host.profiles
+*/
