5497f58a1c94aa4352786824867d780e964ae449
Drewry Pope <drewrypope@gmail.com>
2024-10-24 01:36:07 -0500

    amd 82 current  2024-10-24 01:35:59  24.11.20241014.b36c6a9  6.6.56                          *

--- home/user/default.nix
+++ home/user/default.nix
@@ -67,8 +67,9 @@
         format = "<b>%s</b>\\n%b";
         backgroundColor = "#303030FF";
         borderColor = "#333333FF";
+        # on-button-right=exec makoctl menu -n "$id" rofi -dmenu -p 'Select action: '
         extraConfig = ''
-          on-button-right=exec makoctl menu -n "$id" rofi -dmenu -p 'Select action: '
+          on-button-right=exec hyprctl setprop pid:$id
           [urgency=low]
           default-timeout=3000
 
