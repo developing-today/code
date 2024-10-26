b3591548e243406987f6166e6262dc23601151cf
Drewry Pope <drewrypope@gmail.com>
2024-10-26 04:00:29 -0500

    amd 100 current  2024-10-26 04:00:22  24.11.20241026.07ebbba  6.6.58                          *

--- hosts/systemd/backup/default.nix
+++ hosts/systemd/backup/default.nix
@@ -4,6 +4,6 @@
 {
   systemd.tmpfiles.rules = [
     "d /home/backup/.config/oils 0755 user users"
-    "d /home/backup/.local/share/oils 0755 user group"
+    "d /home/backup/.local/share/oils 0755 user users"
   ];
 }
