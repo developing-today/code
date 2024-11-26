91831cea9d054186402a732b7f11caff0e18a45c
Drewry Pope <drewrypope@gmail.com>
2024-11-26 01:24:37 -0600

    amd 185 current  2024-11-26 01:24:20  24.11.20241114.116eed8  6.6.60                          *

--- hosts/networking/enp195s0f3u1/default.nix
+++ hosts/networking/enp195s0f3u1/default.nix
@@ -17,7 +17,14 @@
     externalInterface = "wlp1s0";
     internalInterfaces = [ "enp195s0f3u1" ];
   };
-
+  services.dnsmasq = {
+    enable = true;
+    settings = {
+      interface = "enp195s0f3u1";
+      "dhcp-range" = "10.0.42.2,10.0.42.254,24h";
+      "bind-dynamic" = true;  # Try this instead of bind-interfaces
+    };
+  };
   # services.dnsmasq = {
   #   enable = true;
   #   settings = {
