b0172727d66b83ab32fc988936e446de09ef04c0
Drewry Pope <drewrypope@gmail.com>
2024-11-26 01:41:10 -0600

    amd 186 current  2024-11-26 01:40:59  24.11.20241114.116eed8  6.6.60                          *

--- hosts/networking/enp195s0f3u1/default.nix
+++ hosts/networking/enp195s0f3u1/default.nix
@@ -1,46 +1,35 @@
 { ... }:
 {
-  networking.interfaces.enp195s0f3u1 = {
-    ipv4.addresses = [{
-      address = "10.0.42.1";
-      prefixLength = 24;
-    }];
-    # Make sure interface comes up automatically
-    wakeOnLan.enable = true;
-    useDHCP = false;
-  };
-
   boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
-
-  networking.nat = {
-    enable = true;
-    externalInterface = "wlp1s0";
-    internalInterfaces = [ "enp195s0f3u1" ];
+  networking = {
+    firewall = {
+      enable = true;
+      allowedUDPPorts = [ 53 67 68 ];
+      allowedTCPPorts = [ 53 ];
+    };
+    interfaces.enp195s0f3u1 = {
+      ipv4.addresses = [{
+        address = "10.0.42.1";
+        prefixLength = 24;
+      }];
+      wakeOnLan.enable = true;
+      useDHCP = false;
+    };
+    nat = {
+      enable = true;
+      externalInterface = "wlp1s0";
+      internalInterfaces = [ "enp195s0f3u1" ];
+    };
   };
   services.dnsmasq = {
     enable = true;
     settings = {
-      interface = "enp195s0f3u1";
-      "dhcp-range" = "10.0.42.2,10.0.42.254,24h";
-      "bind-dynamic" = true;  # Try this instead of bind-interfaces
+      interface = [ "enp195s0f3u1" "enp193s0f3u1" ];
+      "dhcp-range" = [
+        "enp195s0f3u1,10.0.42.2,10.0.42.254,24h"
+        "enp193s0f3u1,10.0.42.2,10.0.42.254,24h"
+      ];
+      "bind-dynamic" = true;
     };
-  };
-  # services.dnsmasq = {
-  #   enable = true;
-  #   settings = {
-  #     interface = "enp195s0f3u1";
-  #     "dhcp-range" = "10.0.42.2,10.0.42.254,24h";
-  #     "bind-interfaces" = true;
-  #     "listen-address" = "10.0.42.1";
-  #     "domain-needed" = true;
-  #     "bogus-priv" = true;
-  #   };
-  # };
-
-  # Ensure DHCP ports are open
-  networking.firewall = {
-    enable = true;
-    allowedUDPPorts = [ 53 67 68 ];  # DNS, DHCP ports
-    allowedTCPPorts = [ 53 ];     # DNS
   };
 }
