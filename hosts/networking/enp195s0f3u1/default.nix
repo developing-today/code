0dcf8a0d10a5a027e15606babd2298dc7b5de657
Drewry Pope <drewrypope@gmail.com>
2024-11-26 01:49:39 -0600

    amd 187 current  2024-11-26 01:47:53  24.11.20241114.116eed8  6.6.60                          *

--- hosts/networking/enp195s0f3u1/default.nix
+++ hosts/networking/enp195s0f3u1/default.nix
@@ -1,13 +1,9 @@
 { ... }:
-{
-  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
-  networking = {
-    firewall = {
-      enable = true;
-      allowedUDPPorts = [ 53 67 68 ];
-      allowedTCPPorts = [ 53 ];
-    };
-    interfaces.enp195s0f3u1 = {
+let
+  externalInterface = "wlp1s0";
+  internalInterfaces = [ "enp195s0f3u1" "enp193s0f3u1" ];
+  makeIpConfig = interface: {
+    "${interface}" = {
       ipv4.addresses = [{
         address = "10.0.42.1";
         prefixLength = 24;
@@ -15,21 +11,32 @@
       wakeOnLan.enable = true;
       useDHCP = false;
     };
-    nat = {
-      enable = true;
-      externalInterface = "wlp1s0";
-      internalInterfaces = [ "enp195s0f3u1" ];
-    };
   };
-  services.dnsmasq = {
-    enable = true;
-    settings = {
-      interface = [ "enp195s0f3u1" "enp193s0f3u1" ];
-      "dhcp-range" = [
-        "enp195s0f3u1,10.0.42.2,10.0.42.254,24h"
-        "enp193s0f3u1,10.0.42.2,10.0.42.254,24h"
-      ];
-      "bind-dynamic" = true;
-    };
-  };
+  makeDhcpRange = interface: "${interface},10.0.42.2,10.0.42.254,24h";
+in
+{
+ boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+
+ networking = {
+   firewall = {
+     enable = true;
+     allowedUDPPorts = [ 53 67 68 ];
+     allowedTCPPorts = [ 53 ];
+   };
+   interfaces = builtins.foldl' (acc: interface: acc // makeIpConfig interface) {} internalInterfaces;
+   nat = {
+     enable = true;
+     externalInterface = externalInterface;
+     internalInterfaces = internalInterfaces;
+   };
+ };
+
+ services.dnsmasq = {
+   enable = true;
+   settings = {
+     interface = internalInterfaces;
+     dhcp-range = map makeDhcpRange internalInterfaces;
+     bind-dynamic = true;
+   };
+ };
 }
