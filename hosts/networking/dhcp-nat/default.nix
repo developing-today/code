45f38a87ca2b70ef33c0e993eb4637e8518e7c3b
Drewry Pope <drewrypope@gmail.com>
2024-11-26 04:25:39 -0600

    stuff

--- hosts/networking/dhcp-nat/default.nix
+++ hosts/networking/dhcp-nat/default.nix
@@ -1,74 +1,83 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 let
- externalInterface = "wlp1s0";
- internalInterfaces = [
-   "enp193s0f3u1"
-   "enp195s0f3u1"
- ];
- interfaceIndices = builtins.listToAttrs (
-   lib.lists.imap0 (idx: interface: { name = interface; value = idx; }) internalInterfaces
- );
- networkBase = index: {
-   prefix = "10.0.${toString index}";
-   gateway = "0";
-   dhcpStart = "1";
-   dhcpEnd = "254";
-   prefixLength = 24;
-   netmask = "255.255.255.0";
- };
- makeIpConfig = interface: {
-   "${interface}" = {
-     ipv4.addresses = [
-       {
-         address = "${(networkBase interfaceIndices.${interface}).prefix}.${(networkBase interfaceIndices.${interface}).gateway}";
-         prefixLength = (networkBase interfaceIndices.${interface}).prefixLength;
-       }
-     ];
-     wakeOnLan.enable = true;
-     useDHCP = false;
-   };
- };
-makeDhcpRange = index: interface:
-  "${interface},${(networkBase index).prefix}.${(networkBase index).dhcpStart},${(networkBase index).prefix}.${(networkBase index).dhcpEnd},${(networkBase index).netmask},24h";
-  # ACTION=="add", SUBSYSTEM=="net", INTERFACE=="${interface}", RUN+="${pkgs.systemd}/bin/systemctl restart network-addresses-${interface}.service"
-# makeUdevRule = interface: ''
-#   SUBSYSTEM=="net", ACTION=="add|move", NAME=="${interface}", RUN+="/run/current-system/sw/bin/logger 'udev rule triggered for ${interface}'", RUN+="${pkgs.systemd}/bin/systemctl restart network-addresses-${interface}.service"
-# '';
-makeUdevRule = interface: ''
-  SUBSYSTEM=="net", ACTION=="add|move", NAME=="${interface}", TAG+="systemd", ENV{SYSTEMD_WANTS}="network-addresses-${interface}.service"
-'';
+  externalInterface = "wlp1s0";
+  internalInterfaces = [
+    "enp193s0f3u1"
+    "enp195s0f3u1"
+  ];
+  interfaceIndices = builtins.listToAttrs (
+    lib.lists.imap0 (idx: interface: {
+      name = interface;
+      value = idx;
+    }) internalInterfaces
+  );
+  networkBase = index: {
+    prefix = "10.0.${toString index}";
+    gateway = "0";
+    dhcpStart = "1";
+    dhcpEnd = "254";
+    prefixLength = 24;
+    netmask = "255.255.255.0";
+  };
 in
 {
- boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
- networking = {
-   usePredictableInterfaceNames = true;
-   firewall = {
-     enable = true;
-     allowedUDPPorts = [
-       53
-       67
-       68
-     ];
-     allowedTCPPorts = [ 53 ];
-   };
-   interfaces = builtins.foldl' (acc: interface: acc // makeIpConfig interface) { } internalInterfaces;
-   nat = {
-     enable = true;
-     externalInterface = externalInterface;
-     internalInterfaces = internalInterfaces;
-   };
- };
- services = {
-   dnsmasq = {
-     enable = true;
-     settings = {
-       interface = internalInterfaces;
-       dhcp-range = builtins.genList (i: makeDhcpRange i (builtins.elemAt internalInterfaces i)) (
-         builtins.length internalInterfaces
-       );
-       bind-dynamic = true;
-     };
-   };
-   udev.extraRules = lib.concatMapStrings makeUdevRule internalInterfaces;
- };
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  networking = {
+    usePredictableInterfaceNames = true;
+    firewall = {
+      enable = true;
+      allowedUDPPorts = [
+        53
+        67
+        68
+      ];
+      allowedTCPPorts = [ 53 ];
+    };
+    interfaces = builtins.foldl' (
+      acc: interface:
+      acc
+      //
+        (interface: {
+          "${interface}" = {
+            ipv4.addresses = [
+              {
+                address = "${(networkBase interfaceIndices.${interface}).prefix}.${
+                  (networkBase interfaceIndices.${interface}).gateway
+                }";
+                prefixLength = (networkBase interfaceIndices.${interface}).prefixLength;
+              }
+            ];
+            wakeOnLan.enable = true;
+            useDHCP = false;
+          };
+        })
+          interface
+    ) { } internalInterfaces;
+    nat = {
+      enable = true;
+      externalInterface = externalInterface;
+      internalInterfaces = internalInterfaces;
+    };
+  };
+  services = {
+    dnsmasq = {
+      enable = true;
+      settings = {
+        interface = internalInterfaces;
+        dhcp-range = builtins.genList (
+          i:
+          (
+            index: interface:
+            "${interface},${(networkBase index).prefix}.${(networkBase index).dhcpStart},${(networkBase index).prefix}.${(networkBase index).dhcpEnd},${(networkBase index).netmask},24h"
+          )
+            i
+            (builtins.elemAt internalInterfaces i)
+        ) (builtins.length internalInterfaces);
+        bind-dynamic = true;
+      };
+    };
+    udev.extraRules = lib.concatMapStrings (interface: ''
+      SUBSYSTEM=="net", ACTION=="add|move", NAME=="${interface}", TAG+="systemd", ENV{SYSTEMD_WANTS}="network-addresses-${interface}.service"
+    '') internalInterfaces;
+  };
 }
