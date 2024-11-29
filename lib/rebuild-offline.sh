1ba90165024ea3636a6aca8bc41325619dc7f8c1
Drewry Pope <drewrypope@gmail.com>
2024-11-28 22:35:50 -0600

    amd 211 current  2024-11-28 22:35:36  24.11.20241114.116eed8  6.6.60                          *

--- lib/rebuild-offline.sh
+++ lib/rebuild-offline.sh
@@ -1,2 +1,35 @@
-#!/usr/bin/env bash
+##!/usr/bin/env bash
+set -exuo pipefail
+#ulimit -n $(ulimit -Hn)
+#sudo prlimit --pid $$ --nofile=1000000:1000000
+#nix-shell -p nixVersions.nix_2_18 git cachix jq
+#cat /mnt/c/wsl/cachix.key | cachix authtoken --stdin
+# Get the directory of the script
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Git add for the script's directory
+cd "${script_dir}" || exit 1
+echo "entered: $script_dir"
+echo "git add ."
+
+git add .
+if systemctl is-active --quiet tailscaled; then # this is a hack: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-2134547782
+  echo "stopping tailscaled..." # this is a hack: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-2134547782
+  sudo systemctl stop tailscaled # this is a hack: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-2134547782
+  echo "tailscaled service stopped." # this is a hack: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-2134547782
+else
+  echo "tailscaled service not found or not active." # hack not needed
+fi
+# --refresh
+# --offline
+# --no-build-nix // --fast
+# --use-substitutes
+# --no-net
+# -vvvv
 nixos-rebuild --use-remote-sudo --offline --no-net --accept-flake-config --json switch --json --upgrade --json --print-build-logs --verbose --keep-going --log-format internal-json --fallback --show-trace --flake '.' |& nom --json
+current=$(nixos-rebuild list-generations | grep current)
+echo "current: $current"
+hostname=$(hostname)
+echo "hostname: $hostname"
+git commit --no-verify --allow-empty -m "$hostname $current"
+# don't add after rebuild to prevent mismatched file content vs version
