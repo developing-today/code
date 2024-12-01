3be92b9e8fefea6c285b267ad0452dd2e7582795
Drewry Pope <drewrypope@gmail.com>
2024-12-01 07:25:36 -0600

    amd 245 current  2024-12-01 07:25:21  24.11.20241129.3d7df75  6.6.60                          *

--- lib/default.nix
+++ lib/default.nix
@@ -414,19 +414,19 @@ let
       inherit (clan) clanInternals; # nixosConfigurations clanInternals;
       # Add the Clan cli tool to the dev shell.
       # Use "nix develop" to enter the dev shell.
-      # devShells =
-      #   inputs.clan-core.inputs.nixpkgs.lib.genAttrs
-      #     [
-      #       "x86_64-linux"
-      #       # "aarch64-linux"
-      #       # "aarch64-darwin"
-      #       # "x86_64-darwin"
-      #     ]
-      #     (system: {
-      #       default = inputs.clan-core.inputs.nixpkgs.legacyPackages.${system}.mkShell {
-      #         packages = [ inputs.clan-core.packages.${system}.clan-cli ];
-      #       };
-      #     });
+      devShells =
+        inputs.clan-core.inputs.nixpkgs.lib.genAttrs
+          [
+            "x86_64-linux"
+            # "aarch64-linux"
+            # "aarch64-darwin"
+            # "x86_64-darwin"
+          ]
+          (system: {
+            default = inputs.clan-core.inputs.nixpkgs.legacyPackages.${system}.mkShell {
+              packages = [ inputs.clan-core.packages.${system}.clan-cli ];
+            };
+          });
     };
 
   self = merge [
