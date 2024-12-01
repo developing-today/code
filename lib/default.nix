6e9d19fbd99700839eb504901db02982cc4e0906
Drewry Pope <drewrypope@gmail.com>
2024-12-01 09:25:54 -0600

    amd 247 current  2024-12-01 09:25:39  24.11.20241129.3d7df75  6.6.60                          *

--- lib/default.nix
+++ lib/default.nix
@@ -404,8 +404,8 @@ let
         # local> Edit ./machines/<machine>/configuration.nix to your liking.
         machines = {
           # The name will be used as hostname by default.
-          # jon = { };
-          # sara = { };
+          jon = { };
+          sara = { };
         };
       };
     in
