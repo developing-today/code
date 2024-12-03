a352478867ee73183c98219423755e1770482c87
Drewry Pope <drewrypope@gmail.com>
2024-12-02 18:45:41 -0600

    252

--- machines/user/configuration.nix
+++ machines/user/configuration.nix
@@ -14,7 +14,7 @@
   # Set this for clan commands use ssh i.e. `clan machines update`
   # If you change the hostname, you need to update this line to root@<new-hostname>
   # This only works however if you have avahi running on your admin machine else use IP
-  clan.core.networking.targetHost = "root@10.0.0.251";
+  clan.core.networking.targetHost = "root@10.0.0.252";
 
   # You can get your disk id by running the following command on the installer:
   # Replace <IP> with the IP of the installer printed on the screen or by running the `ip addr` command.
