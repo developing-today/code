348c1ed8acd85b65bd314eceaf928daa9ffa34ae
Drewry Pope <drewrypope@gmail.com>
2024-12-04 00:08:50 -0600

    amd 259 current  2024-12-04 00:08:37  24.11.20241129.3d7df75  6.6.60                          *

--- nixos/nix/default.nix
+++ nixos/nix/default.nix
@@ -15,7 +15,8 @@
     registry = lib.mkForce (lib.mapAttrs (_: value: { flake = value; }) inputs); # This will add each flake input as a registry. To make nix3 commands consistent with your flake
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry; # This will additionally add your inputs to the system's legacy channels. Making legacy nix commands consistent as well, awesome!
     # package = pkgs.nixVersions.nix_2_23;
-    package = pkgs.nixVersions.git;
+    package = pkgs.nixVersions.nix_2_24;
+    # package = pkgs.nixVersions.git;
     # optimise.automatic = true;
     # gc = {
     #   automatic = true;
