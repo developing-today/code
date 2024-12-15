088bae9d130fe0a88b867a0149a87dbba97477e2
Drewry Pope <drewrypope@gmail.com>
2024-12-15 00:12:51 -0600

    amd 272 current  2024-12-15 00:12:38  24.11.20241129.3d7df75  6.6.60                          nvidia-egpu

--- nixos/specialisations/nvidia-egpu/default.nix
+++ nixos/specialisations/nvidia-egpu/default.nix
@@ -1,20 +1,39 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  inputs,
+  config,
+  ...
+}:
 {
   specialisation = {
     nvidia-egpu.configuration = {
+      imports = [ inputs.nixos-hardware.nixosModules.common-gpu-nvidia ];
+      # boot.kernelPackages = pkgs.linuxPackages_6_1; # EOL 2026 | 2033 CIP # https://github.com/133760D/Nix-nvidia-configuration/blob/main/boot_kernel.nix
       boot.initrd.kernelModules = [
         "nvidia"
-        "i915"
+        # "i915"
         "nvidia_modeset"
         # "nvidia_uvm"
         "nvidia_drm"
       ];
+
+      # boot.blacklistedKernelModules = [ "nouveau" ];
+      # boot.kernelParams = [ "module_blacklist=amdgpu" ];
+      # boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
       system.nixos.tags = [ "nvidia-egpu" ];
       hardware.graphics = {
         enable = true;
         enable32Bit = true;
         extraPackages = with pkgs; [ vaapiVdpau ];
+
+        # opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+        # opengl.setLdLibraryPath = true;
       };
+      # environment.systemPackages = with pkgs; [
+      #     vulkan-tools
+      #     vulkan-loader
+      #     vulkan-validation-layers
+      #   ];
       # hardware.opengl = {
       #   enable = true;
       #   driSupport = true;
@@ -22,6 +41,16 @@
       # };
       # nvidia-x11, nvidia-settings, and nvidia-persistenced.
       services.xserver.videoDrivers = [ "nvidia" ];
+      systemd.services.nvidia-persistenced = {
+        enable = true;
+        description = "NVIDIA Persistence Daemon";
+        after = [ "multi-user.target" ];
+        wantedBy = [ "multi-user.target" ];
+      };
+      # environment.systemPackages = with pkgs; [
+      #   # nvidia-settings
+      #   nvidia-x11
+      # ];
       hardware.nvidia = {
         #
         # export __NV_PRIME_RENDER_OFFLOAD=1
@@ -33,12 +62,16 @@
         # 	enable = true;
         # 	enableOffloadCmd = true;
         # };
+        forceFullCompositionPipeline = true;
         modesetting.enable = true;
         powerManagement.enable = false;
         powerManagement.finegrained = false;
         open = false;
         nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.latest;
+        # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/os-specific/linux/nvidia-x11/default.nix
+        package = config.boot.kernelPackages.nvidiaPackages.production;
+        # package = config.boot.kernelPackages.nvidiaPackages.
+        # pf
         nvidiaPersistenced = true;
         prime = {
           # sync.enable = true;
