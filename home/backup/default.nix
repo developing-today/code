f6617451cd781baeb8f10b8ad6e16d17fa4df0e4
Drewry Pope <drewrypope@gmail.com>
2024-12-19 23:08:57 -0600

    etc

--- home/backup/default.nix
+++ home/backup/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  inputs,
   stateVersion,
   pkgs,
   ...
@@ -7,6 +8,10 @@
 {
   imports = [ (lib.from-root "nixos/home") ];
   home-manager.users.backup = {
+    wayland.windowManager.hyprland = {
+      enable = true;
+      plugins = [ inputs.hypr-dynamic-cursors.packages.${pkgs.system}.hypr-dynamic-cursors ];
+    };
     # TODO: ensure home manager standalone can still work
     # TODO: factor out modules into shared files
     nixpkgs.config = {
