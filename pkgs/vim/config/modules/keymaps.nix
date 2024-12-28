f8c0612eff11f58c21d48f23a00f0ad5523b9428
Drewry Pope <drewrypope@gmail.com>
2024-12-28 00:09:26 -0600

    amd 323 current  2024-12-28 00:09:11  25.05.20241221.a168813              6.6.67                          *

--- pkgs/vim/config/modules/keymaps.nix
+++ pkgs/vim/config/modules/keymaps.nix
@@ -13,5 +13,38 @@
       };
       action = ":noh<CR>";
     }
+
+      {
+        mode = ["n" "i"];  # Both normal and insert modes
+        key = "<C-1>";
+        action = "<C-j>";
+        options = {
+          silent = true;
+        };
+      }
+      {
+        mode = ["n" "i"];  # Both normal and insert modes
+        key = "<C-2>";
+        action = "<C-k>";
+        options = {
+          silent = true;
+        };
+      }
+      {
+        mode = ["n" "i"];  # Both normal and insert modes
+        key = "<C-3>";
+        action = "<C-l>";
+        options = {
+          silent = true;
+        };
+      }
+      {
+        mode = ["n" "i"];  # Both normal and insert modes
+        key = "<C-4>";
+        action = "<C-m>";
+        options = {
+          silent = true;
+        };
+      }
   ];
 }
