1772570d0b7806d9485df8c6465309bffc6a907e
Drewry Pope <drewrypope@gmail.com>
2024-12-30 23:27:37 -0600

    amd 333 current  2024-12-30 23:27:21  25.05.20241221.a168813              6.6.67                          *

--- home/user/default.nix
+++ home/user/default.nix
@@ -13,7 +13,37 @@
       enable = true;
       plugins = [ inputs.hypr-dynamic-cursors.packages.${pkgs.system}.hypr-dynamic-cursors ];
       extraConfig = builtins.readFile (lib.from-root "config/hypr/hyprland.conf");
+      # wayland.windowManager.hyprland.settings = {
+      #   "$mod" = "SUPER";
+      # wayland.windowManager.hyprland.systemd.variables = ["--all"];
     };
+
+    # home.pointerCursor = {
+    #   gtk.enable = true;
+    #   # x11.enable = true;
+    #   package = pkgs.bibata-cursors;
+    #   name = "Bibata-Modern-Classic";
+    #   size = 16;
+    # };
+
+    # gtk = {
+    #   enable = true;
+
+    #   theme = {
+    #     package = pkgs.flat-remix-gtk;
+    #     name = "Flat-Remix-GTK-Grey-Darkest";
+    #   };
+
+    #   iconTheme = {
+    #     package = pkgs.gnome.adwaita-icon-theme;
+    #     name = "Adwaita";
+    #   };
+
+    #   font = {
+    #     name = "Sans";
+    #     size = 11;
+    #   };
+    # };
     # TODO: ensure home manager standalone can still work
     # TODO: factor out modules into shared files
     nixpkgs.config = {
