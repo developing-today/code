2dcbe5e1b0a1d9090728a71a9f971067e5986876
Drewry Pope <drewrypope@gmail.com>
2025-07-09 03:03:15 -0500

    rmsqlite

--- pkgs/roc/platforms/rust-minimal-cli/flake.nix
+++ pkgs/roc/platforms/rust-minimal-cli/flake.nix
@@ -15,9 +15,24 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, roc, rust-overlay, flake-utils }:
-    let supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-    in flake-utils.lib.eachSystem supportedSystems (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      roc,
+      rust-overlay,
+      flake-utils,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
@@ -27,53 +42,48 @@
 
         # get current working directory
         cwd = builtins.toString ./.;
-        rust =
-          pkgs.rust-bin.fromRustupToolchainFile "${toString ./rust-toolchain.toml}";
+        rust = pkgs.rust-bin.fromRustupToolchainFile "${toString ./rust-toolchain.toml}";
 
         aliases = ''
           alias buildcmd='bash jump-start.sh && roc ./build.roc -- --roc roc'
           alias testcmd='export ROC=roc && export EXAMPLES_DIR=./examples/ && ./ci/all_tests.sh'
         '';
 
-        linuxInputs = with pkgs;
-          lib.optionals stdenv.isLinux [
-            valgrind
-          ];
+        linuxInputs = with pkgs; lib.optionals stdenv.isLinux [ valgrind ];
 
-        darwinInputs = with pkgs;
-          lib.optionals stdenv.isDarwin
-          (with pkgs.darwin.apple_sdk.frameworks; [
-            Security
-          ]);
+        darwinInputs =
+          with pkgs;
+          lib.optionals stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [ Security ]);
 
-        sharedInputs = (with pkgs; [
-          sqlite
-          jq
-          rust
-          llvmPkgs.clang
-          llvmPkgs.lldb # for debugging
-          expect
-          nmap
-          simple-http-server
-          rocPkgs.cli
-          ripgrep # for ci/check_all_exposed_funs_tested.roc
-        ]);
-      in {
+        sharedInputs = (
+          with pkgs;
+          [
+            jq
+            rust
+            llvmPkgs.clang
+            llvmPkgs.lldb # for debugging
+            expect
+            nmap
+            simple-http-server
+            rocPkgs.cli
+            ripgrep # for ci/check_all_exposed_funs_tested.roc
+          ]
+        );
+      in
+      {
 
         devShell = pkgs.mkShell {
           buildInputs = sharedInputs ++ darwinInputs ++ linuxInputs;
 
           # nix does not store libs in /usr/lib or /lib
           # for libgcc_s.so.1
-          NIX_LIBGCC_S_PATH =
-            if pkgs.stdenv.isLinux then "${pkgs.stdenv.cc.cc.lib}/lib" else "";
+          NIX_LIBGCC_S_PATH = if pkgs.stdenv.isLinux then "${pkgs.stdenv.cc.cc.lib}/lib" else "";
           # for crti.o, crtn.o, and Scrt1.o
-          NIX_GLIBC_PATH =
-            if pkgs.stdenv.isLinux then "${pkgs.glibc.out}/lib" else "";
+          NIX_GLIBC_PATH = if pkgs.stdenv.isLinux then "${pkgs.glibc.out}/lib" else "";
 
           shellHook = ''
             ${aliases}
-            
+
             echo "Some convenient command aliases:"
             echo "${aliases}" | grep -E "alias [^=]+" -o | sed 's/alias /  /' | sort
             echo ""
@@ -81,5 +91,6 @@
         };
 
         formatter = pkgs.nixpkgs-fmt;
-      });
+      }
+    );
 }
