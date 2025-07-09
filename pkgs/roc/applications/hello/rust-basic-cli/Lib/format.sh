d1da8a8768a6390cab7dae33de6abffc23a16dc5
Drewry Pope <drewrypope@gmail.com>
2025-07-09 03:35:57 -0500

    a

--- pkgs/roc/applications/hello/rust-basic-cli/Lib/format.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -Eexuo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail
-shopt -s globstar nullglob
-files=(**/*.roc *.roc)
-roc format "${files[@]}"
--- pkgs/roc/applications/hello/rust-minimal-cli/Lib/main.roc
+++
@@ -1,5 +0,0 @@
-package [
-    Display,
-    Hello,
-    Square,
-] {}
