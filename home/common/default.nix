5c5bedbdccb00e9a3c7ee66a42914c72680bb433
Drewry Pope <drewrypope@gmail.com>
2026-02-15 02:31:11 -0600

    amd 433

--- home/common/default.nix
+++ home/common/default.nix
@@ -387,7 +387,10 @@
     #ripgrep.enable = true;
     rtorrent.enable = true;
     # sagemath.enable = true; # oh my god 1 hour + build times and then it usually fails. if it's cached you're fine but on unstable it is just not always cached. even worse against master branch
-    ssh.enable = true;
+    ssh = {
+      enable = true;
+      enableDefaultConfig = true;
+    };
     starship.enable = true;
     swaylock.enable = true;
     taskwarrior = {
